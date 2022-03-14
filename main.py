feeba431a6fa62edef93bce75c5c7dc4b8bedaf4
QWERTYpy <burtsevmv@mail.ru>
2022-03-14 17:12:30 +0300

    Выявлена некорректная работа программы. Нужен дополнительный анализ

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ def open_file():
     lab_f_count["text"] = len(filepath)
     lab_o_count["text"] = 0
     but_start['text'] = 'Старт'
+    but_ffmpeg['text'] = 'Ffmpeg det'
 
 
 def start(flag=True):
@@ -191,6 +192,11 @@ def ffmpeg_det():
         height_ff = str((xy_coord[1][0] - xy_coord[0][0]) * frame_zoom)
         x_ff = str(xy_coord[0][0] * frame_zoom)
         y_ff = str(xy_coord[0][1] * frame_zoom)
+        def ff_time(ff_t):
+            h = int(ff_t // 3600)
+            m = int((ff_t // 60) % 60)
+            s = int(ff_t % 60)
+            return "%02d:%02d:%02d" % (h, m, s)
 
         for file_path_id in range(int(lab_o_count['text']), len(filepath)):
             file_path = filepath[file_path_id]
@@ -198,6 +204,78 @@ def ffmpeg_det():
             os.system('ffmpeg -i '+file_path+' -vf "crop='+width_ff+':'+height_ff+':'+x_ff+':'+y_ff+",select='gt(scene,0.00"+
                       sens_ff+")',"+'setpts=N/(25*TB)" -y '+ file_path[:-4] +
                       '_crop_detect' + file_path[len(file_path) - 4:])
+            """
+            os.system('ffmpeg -i ' + file_path + ' -vf "crop=' + width_ff + ':' + height_ff + ':' + x_ff + ':' + y_ff +
+                      ",select='gt(scene,0.00" + sens_ff + ")'," + 'showinfo" -f null - > '+file_path+'.txt 2>&1')
+            if os.path.exists(file_path+'.txt'):
+                print('Существует')
+                file_inf = open(file_path+'.txt', 'r')
+                sec_inf = []
+                for line in file_inf:
+                    if "pts_time:" in line:
+                        sec_inf.append(int(float(line[line.find("pts_time:")+9:line.find("pos:")])))
+
+                print(sec_inf)
+                for id in list(sec_inf):
+                    if sec_inf.count(id) > 1:
+                        sec_inf.remove(id)
+
+                print(sec_inf)
+                sec_line = []
+                sec_id = 0
+                print('Длина', len(sec_inf))
+                #print(sec_inf[64])
+                while sec_id < len(sec_inf):
+                    #print(sec_id)
+                    left_sec = sec_inf[sec_id]
+                    while sec_inf[sec_id] - left_sec < 1:
+                        if sec_id < len(sec_inf)-1:
+                            sec_id+=1
+                        else:
+                            sec_id+=1
+                            break
+                    sec_id-=1
+                    right_sec = sec_inf[sec_id]
+                    sec_line.append([left_sec, right_sec])
+                    sec_id += 1
+                print(sec_line)
+
+                def ff_rec():
+                    #global sec_line
+                    print(sec_line)
+                    for ff_list_id in range(0, len(sec_line)):
+                        if ff_list_id == len(sec_line) - 1:
+                            return sec_line
+                        if sec_line[ff_list_id + 1][0] - sec_line[ff_list_id][1] <= 2:
+                            sec_line.insert(ff_list_id, [sec_line[ff_list_id][0], sec_line[ff_list_id + 1][1]])
+                            sec_line.pop(ff_list_id + 1)
+                            sec_line.pop(ff_list_id + 1)
+                            break
+                    ff_rec()
+
+                ff_rec()
+                print(sec_line)
+                print(sec_line)
+                for ss, to in sec_line:
+                    print(ff_time(ss), ff_time(to+1))
+                    os.system('ffmpeg -ss '+ff_time(ss)+' -to '+ff_time(to+1)+' -i '+file_path+' -c copy -y '+file_path[:-4] +
+                    str("%03d" % sec_line.index([ss, to]))+ "ff_tmp" + file_path[len(file_path) - 4:])
+                    #ffmpeg -ss 00:00:01 -to 00:00:02 -i pr.avi -c copy -y out2.avi
+
+            my_file = open("list.txt", "w+")  # Создаем файл для хранения имен файлов для объединения
+            for name_file in os.listdir(os.path.dirname(file_path)):
+                if 'ff_tmp' in name_file:
+                    my_file.write("file '" + os.path.dirname(file_path) + "/" + name_file + "'\n")
+            my_file.close()
+            os.system('ffmpeg -f concat -safe 0 -i list.txt -c copy -y ' + file_path[:-4] +
+                    '_all_result' + file_path[len(file_path) - 4:])
+            os.remove('list.txt')
+            file_inf.close()
+            os.remove(file_path+'.txt')
+            for name_file in os.listdir(os.path.dirname(file_path)):
+                if 'ff_tmp' in name_file:
+                    os.remove(os.path.dirname(file_path) + "/" + name_file)
+
             lab_o_count["text"] = filepath.index(file_path) + 1
             window.update()
             end_detect = time.time()  # Время завершения обработки видео файла
@@ -206,6 +284,7 @@ def ffmpeg_det():
             # ffmpeg -i test.avi -vf "crop=300:300:1200:200,select='gt(scene,0.009)',setpts=N/(25*TB)" -y out2.mp4
             # ffmpeg -i pr.avi -vf "crop=300:300:740:300,select='gt(scene,0.004)',showinfo" -f null - > cor.log 2>&1
             # Если стоит отметка об объединении и конвертирован последний файл, то запустить объединение
+            """
         but_ffmpeg['text'] = 'Готово'
         window.update()
         #if chk_cut.get() and len(filepath) == filepath.index(file_path) + 1:
